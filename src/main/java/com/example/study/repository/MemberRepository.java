9a01e6225fbb8b20e693095e45ed5b2198a03aeb
Dasom04 <slz_952@naver.com>
2024-05-27 13:05:09 +0900

    #2. QueryDSL의 다양한 기초 문법들

---
+++ src/main/java/com/example/study/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.example.study.repository;
+
+import com.example.study.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+// JQPL방식
+//    @Query("SELECT m FROM MEMBER m WHERE m.userName = :userName")
+//    Optional<Member> findOneMember(@Param("userName") String userName);
+
+}
